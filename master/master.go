82470fd23ecdfb53d565401691c7ffa79411b695
Orel <ryiseld@gmail.com>
2022-03-27 13:07:57 +0300

    Add BadgerDB and test in /get/{key}

--- master/master.go
+++ master/master.go
@@ -5,16 +5,38 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/dgraph-io/badger/v3"
 	"github.com/gorilla/mux"
 )
+
+type Context struct {
+	db *badger.DB
+}
 
 // Start master server
 func Start(port int) {
 	log.Printf("Master server starting on port %v...", port)
 
+	db, err := badger.Open(badger.DefaultOptions("badger"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	context := Context{
+		db,
+	}
+
+	err = db.Update(func(txn *badger.Txn) error {
+		err := txn.Set([]byte("answer"), []byte("42"))
+		return err
+	})
+
 	router := mux.NewRouter()
 	router.HandleFunc("/", indexHandler).Methods("GET")
-	router.HandleFunc("/get/{key}", getKeyHandler).Methods("GET")
+	router.HandleFunc("/get/{key}", func(w http.ResponseWriter, r *http.Request) {
+		getKeyHandler(w, r, &context)
+	}).Methods("GET")
 	http.Handle("/", router)
 	http.ListenAndServe(fmt.Sprintf("localhost:%v", port), router)
 }
@@ -25,7 +47,26 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // Handle retrieveing keys
-func getKeyHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	fmt.Fprintf(w, "Key: %v\n", vars["key"])
+func getKeyHandler(w http.ResponseWriter, r *http.Request, c *Context) {
+	key := mux.Vars(r)["key"]
+
+	err := c.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+
+		err = item.Value(func(v []byte) error {
+			fmt.Fprintf(w, "Key: %v\nValue: %v", key, string(v))
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		fmt.Fprintf(w, "An error occurred while retrieving key \"%v\"", key)
+	}
 }
